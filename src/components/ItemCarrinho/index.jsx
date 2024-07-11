f94341f30ceb06a2bbeea7607aa9bfebc86f8cbd
Pedro Santana <pedromanu27@gmail.com>
2024-07-10 21:14:49 -0300

    refactor: :recycle: Refactor pagina de carrinho

--- src/components/ItemCarrinho/index.jsx
+++ src/components/ItemCarrinho/index.jsx
@@ -3,41 +3,39 @@ import Botao from "@/components/Botao";
 import Quantidade from "@/components/Quantidade";
 import ValorFormatado from "@/components/ValorFormatado";
 import InfoItemCarrinho from "./InfoItemCarrinho";
+import { useCarrinhoContext } from "@/hooks/useCarrinhoContext";
 
-const ItemCarrinho = ({
-  itemCarrinho,
-  removerProdutoCarrinho,
-  adicionarProduto,
-  removerProduto,
-}) => {
-  return (
-    <li key={itemCarrinho.id}>
-      <>
-        <div className="produto">
-          <img
-            className="imagem__produto"
-            src={itemCarrinho.src}
-            alt={itemCarrinho.alt}
-          />
-          <InfoItemCarrinho itemCarrinho={itemCarrinho} />
-          <ValorFormatado valor={itemCarrinho.preco} />
-          <Quantidade
-            itemCarrinho={itemCarrinho}
-            adicionarProduto={adicionarProduto}
-            removerProduto={removerProduto}
-          />
-          <Botao
-            variant="deleteItem"
-            aria-label="Excluir"
-            handleClick={() => removerProdutoCarrinho(itemCarrinho.id)}
-          >
-            delete_forever
-          </Botao>
-        </div>
-        <div className="divisor my-5" />
-      </>
-    </li>
-  );
+const ItemCarrinho = ({ itemCarrinho }) => {
+	const { removerProdutoCarrinho, adicionarProduto, removerProduto } =
+		useCarrinhoContext();
+	return (
+		<li key={itemCarrinho.id}>
+			<>
+				<div className="produto">
+					<img
+						className="imagem__produto"
+						src={itemCarrinho.src}
+						alt={itemCarrinho.alt}
+					/>
+					<InfoItemCarrinho itemCarrinho={itemCarrinho} />
+					<ValorFormatado valor={itemCarrinho.preco} />
+					<Quantidade
+						itemCarrinho={itemCarrinho}
+						adicionarProduto={adicionarProduto}
+						removerProduto={removerProduto}
+					/>
+					<Botao
+						variant="deleteItem"
+						aria-label="Excluir"
+						handleClick={() => removerProdutoCarrinho(itemCarrinho.id)}
+					>
+						delete_forever
+					</Botao>
+				</div>
+				<div className="divisor my-5" />
+			</>
+		</li>
+	);
 };
 
 export default ItemCarrinho;
--- src/pages/Carrinho.jsx
+++ src/pages/Carrinho.jsx
@@ -4,45 +4,35 @@ import Titulo from "@/components/Titulo";
 import Sumario from "@/components/Sumario";
 import ListaProdutosCarrinho from "@/components/ListaProdutosCarrinho";
 import BannerCarrinho from "@/components/BannerCarrinho";
+import { useCarrinhoContext } from "@/hooks/useCarrinhoContext";
 
-const Carrinho = ({
-  carrinho,
-  adicionarProduto,
-  removerProduto,
-  removerProdutoCarrinho,
-  valorTotalCarrinho,
-  quantidadeProdutos,
-}) => {
-  return (
-    <>
-      <BarraNavegacao />
-      <BannerCarrinho />
-      <main className="container-xxl">
-        <Titulo element="h1" className="text-center fw-semibold my-3 my-md-5">
-          Carrinho de Compras
-        </Titulo>
-        <section className="secao__compra text-light mx-md-4 mx-xl-4">
-          <div className="conteudo mb-5">
-            <div className="detalhe__compra bg-black p-4">
-              <Titulo className="text-center fw-bold mb-3 text-md-start">
-                Detalhes da compra
-              </Titulo>
-              <ListaProdutosCarrinho
-                carrinho={carrinho}
-                adicionarProduto={adicionarProduto}
-                removerProduto={removerProduto}
-                removerProdutoCarrinho={removerProdutoCarrinho}
-              />
-            </div>
-            <Sumario
-              valorTotalCarrinho={valorTotalCarrinho}
-              quantidadeProdutos={quantidadeProdutos}
-            />
-          </div>
-        </section>
-      </main>
-    </>
-  );
+const Carrinho = () => {
+	const { valorTotal, quantidade } = useCarrinhoContext();
+	return (
+		<>
+			<BarraNavegacao />
+			<BannerCarrinho />
+			<main className="container-xxl">
+				<Titulo element="h1" className="text-center fw-semibold my-3 my-md-5">
+					Carrinho de Compras
+				</Titulo>
+				<section className="secao__compra text-light mx-md-4 mx-xl-4">
+					<div className="conteudo mb-5">
+						<div className="detalhe__compra bg-black p-4">
+							<Titulo className="text-center fw-bold mb-3 text-md-start">
+								Detalhes da compra
+							</Titulo>
+							<ListaProdutosCarrinho />
+						</div>
+						<Sumario
+							valorTotalCarrinho={valorTotal}
+							quantidadeProdutos={quantidade}
+						/>
+					</div>
+				</section>
+			</main>
+		</>
+	);
 };
 
 export default Carrinho;
